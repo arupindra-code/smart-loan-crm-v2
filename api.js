94f0984430f316164bc9fc3f857a052d6cd0444e
arupindra-code <arupindra6@gmail.com>
2026-07-05 17:38:20 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,25 @@
+const CONFIG = {
+  API_BASE_URL: ""
+};
 
+async function apiGet(endpoint, params = {}) {
+  if (!CONFIG.API_BASE_URL) {
+    throw new Error("API URL not configured.");
+  }
+
+  const url = new URL(CONFIG.API_BASE_URL);
+
+  url.searchParams.set("api", endpoint);
+
+  Object.keys(params).forEach(key => {
+    url.searchParams.set(key, params[key]);
+  });
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("API request failed");
+  }
+
+  return await response.json();
+}
